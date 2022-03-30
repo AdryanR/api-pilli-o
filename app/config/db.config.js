996bebf9a6c0dce0af617e9778022fae7a553b24
adryanR <adryan328@gmail.com>
2022-03-30 14:19:50 -0300

    db config

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "us-cdbr-east-04.cleardb.com",
-  USER: "b6e5897e356418",
-  PASSWORD: "f1b883f9",
-  DB: "heroku_da50ecf521608a2",
+  HOST: "us-cdbr-east-05.cleardb.net",
+  USER: "be68bb7b6201ba",
+  PASSWORD: "79b9c447",
+  DB: "heroku_ebe76991654738d",
   dialect: "mysql",
   pool: {
     max: 5,
@@ -25,3 +25,5 @@ module.exports = {
     idle: 10000
   }
 };*/
+
+// mysql://be68bb7b6201ba:79b9c447@us-cdbr-east-05.cleardb.net/heroku_ebe76991654738d?reconnect=true
