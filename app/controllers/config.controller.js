03c7d3a33e079175f2249a5bc3187cece3ec7ff8
adryanR <adryan328@gmail.com>
2022-06-07 22:20:03 -0300

    Refatoracao de codigo

--- app/controllers/config.controller.js
+++ app/controllers/config.controller.js
@@ -3,41 +3,76 @@ const db = require("../models");
 exports.UserConfig = async (req, res) => {
     const firebaseUserUid = req.body.firebaseUserUid;
 
-    let estaConfigurado = await VerificaResponsavel(firebaseUserUid);
+    let responsavel = await VerificaResponsavel(firebaseUserUid);
 
-    if (estaConfigurado) {
-        res.send("true");
-        return
-    } else {
-        estaConfigurado = await VerificaIdoso(firebaseUserUid);
+    if (responsavel.id) {
+        res.send(responsavel);
+        return;
     }
 
-    if (estaConfigurado) {
-        res.send("true");
-    } else {
-        res.send("false");
-    }
+    let idoso = await VerificaIdoso(firebaseUserUid);
 
+    if (idoso) {
+        res.send(idoso);
+    } else {
+        res.send({});
+    }
 };
 
-
-// Retorna 1 se encontrar: responsável vinculado a idoso, idoso vinculado a máquina e existir o firebaseUserUid
 async function VerificaResponsavel(id) {
-    let userConfig = await db.sequelize.query('SELECT EXISTS( SELECT * FROM responsavels RESP INNER JOIN idosos IDS ON IDS.idResp = RESP.id INNER JOIN maquinas MAQ ON MAQ.id = IDS.idMachine WHERE RESP.firebaseUserUid = :id ) AS estaConfigurado;', {
-        replacements: { id: id },
-        type: db.sequelize.QueryTypes.SELECT
-    });
+    let resultadoResponsavel = await db.sequelize.query(
+        (
+            'SELECT RESP.id, RESP.nome, RESP.login FROM responsavels RESP' +
+            ' INNER JOIN idosos IDS ON IDS.idResp = RESP.id' +
+            ' INNER JOIN maquinas MAQ ON MAQ.id = IDS.idMachine' +
+            ' WHERE RESP.firebaseUserUid = :id'
+        ),
+        {
+            replacements: { id: id },
+            type: db.sequelize.QueryTypes.SELECT
+        },
+    );
 
-    return userConfig[0].estaConfigurado;
+    let responsavel = resultadoResponsavel[0] || {};
+
+    if (responsavel.id) {
+        let resultadoIdosos = await db.sequelize.query(
+            (
+                'SELECT IDS.id, IDS.nome, IDS.login FROM idosos IDS' +
+                ' INNER JOIN maquinas MAQ ON MAQ.id = IDS.idMachine' +
+                ' WHERE IDS.idResp = :idResponsavel'
+            ),
+            {
+                replacements: { idResponsavel: responsavel.id },
+                type: db.sequelize.QueryTypes.SELECT
+            },
+        );
+
+        responsavel.funcao = "responsavel";
+        responsavel.idosos = resultadoIdosos[0];
+    }
+
+    return responsavel;
 };
 
-//  Retorna 1 se encontrar: idoso vinculado a máquina e existir o firebaseUserUid
 async function VerificaIdoso(id) {
-    let userConfig = await db.sequelize.query('SELECT EXISTS( SELECT * FROM idosos IDS INNER JOIN maquinas MAQ ON MAQ.id = IDS.idMachine WHERE IDS.firebaseUserUid = :id ) AS estaConfigurado', {
-        replacements: { id: id },
-        type: db.sequelize.QueryTypes.SELECT
-    });
+    let resultadoIdoso = await db.sequelize.query(
+        (
+            'SELECT IDS.id, IDS.nome, IDS.login FROM idosos IDS' +
+            ' INNER JOIN maquinas MAQ ON MAQ.id = IDS.idMachine' +
+            ' WHERE IDS.firebaseUserUid = :id'
+        ),
+        {
+            replacements: { id: id },
+            type: db.sequelize.QueryTypes.SELECT
+        },
+    );
 
-    return userConfig[0].estaConfigurado;
+    let idoso = resultadoIdoso[0];
+
+    if (idoso.id) {
+        idoso.funcao = "idoso";
+    }
+
+    return idoso;
 };
-
