1ef67a81b90dbb4aa19c1318125a259c6300dee3
Gabriel Morais <gabrieldemorais6@gmail.com>
2022-06-13 09:02:50 -0300

    Alterada configuração de origem do CORS para https em ambiente de desenvolvimento

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  // origin: "http://localhost:8080"
+  // origin: "https://localhost:8080"
   origin: "https://webclient-pillio.herokuapp.com/"
 };
 
